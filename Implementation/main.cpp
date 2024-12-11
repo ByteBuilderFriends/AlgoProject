5bc249fde117910318ec5decc226d2a2ca1bfd67
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-12-11 02:09:25 +0200

    Update main.cpp

--- Implementation/main.cpp
+++ Implementation/main.cpp
@@ -1,4 +1,28 @@
-int main()
-{
+int main() {
+    InventorySystem inventory;
+    int choice;
 
+    do {
+        cout << "1. Add New Product\n";
+        cout << "2. Display Products\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            inventory.addNewProduct();
+            break;
+        case 2:
+            inventory.displayProducts();
+            break;
+        case 3:
+            cout << "Exiting...\n";
+            break;
+        default:
+            cout << "Invalid choice. Try again.\n";
+        }
+    } while (choice != 3);
+
+    return 0;
 }
